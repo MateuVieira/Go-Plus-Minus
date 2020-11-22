8f1a493e7c9942804acec0f95bec18f80d6006a0
Mateus Vieira <mateus17v@gmail.com>
2020-11-22 10:34:51 -0300

    Update - refactor code to teste return

--- plusMinus.go
+++ plusMinus.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Complete the plusMinus function below.
-func plusMinus(arr []int32) {
+func plusMinus(arr []int32) [3]float32 {
 	var results [3]int32
 
 	for _, value := range arr {
@@ -25,13 +25,19 @@ func plusMinus(arr []int32) {
 
 	arrLenght := len(arr)
 
-	for _, value := range results {
-		fmt.Printf("%f\n", (float32(value) / float32(arrLenght)))
+	var output [3]float32
+	for index, value := range results {
+		output[index] = float32(value) / float32(arrLenght)
+		fmt.Printf("%f\n", output[index])
 	}
+
+	return output
 }
 
 func main() {
 	arr := []int32{-4, 3, -9, 0, 4, 1}
 
-	plusMinus(arr)
+	result := plusMinus(arr)
+
+	fmt.Printf("Output: %f", result)
 }
