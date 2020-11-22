d4f7aff6f8a1bdbe8266d5fcac16449ff2ef9e27
Mateus Vieira <mateus17v@gmail.com>
2020-11-22 16:43:10 -0300

    Update - correcting the expected value to use more decimal places

--- plusMinus_test.go
+++ plusMinus_test.go
@@ -11,14 +11,9 @@ func TestDiagonalDifference(t *testing.T) {
 		arr := []int32{-4, 3, -9, 0, 4, 1}
 
 		got := plusMinus(arr)
-		want := [3]float32{0.500000, 0.333333, 0.166667}
+		want := [3]float32{0.500000, 0.333333343267, 0.166666671634}
 
 		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Teste - type got: %s", reflect.TypeOf(got).String())
-			t.Errorf("Teste - type want: %s", reflect.TypeOf(want).String())
-			for index, value := range got {
-				t.Errorf("Teste got: %f - type: %s /  want: %f - type: %s", value, reflect.TypeOf(value).String(), want[index], reflect.TypeOf(want[index]).String())
-			}
 			t.Errorf("got %f want %f given", got, want)
 		}
 	})
